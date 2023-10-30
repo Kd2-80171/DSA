644d2596c55d5aba2cf345781ee6f13b0e5e3167
girishugile <girishugile03@gmail.com>
2023-10-30 23:42:08 +0530

    assignment-04

---
+++ assignment_04/Qsn3/src/com/sunbeam/MiddleOne.java
@@ -0,0 +1,22 @@
+package com.sunbeam;
+class Node{
+	  int data;
+	  Node next;
+	  
+	  public Node (int data) {
+		  this.data = data;
+	  }
+}
+
+public class MiddleOne {
+  class Node{
+	  int data;
+	  Node next;
+	  
+	  public Node (int data) {
+		  this.data = data;
+	  }
+  }
+	
+	
+}
